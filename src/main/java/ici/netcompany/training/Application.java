935d36f45f80b33b102b042901db69f4a353a9e5
Jose Alberto Romero Jimenez <jarjnetcompany@gmail.com>
2018-03-28 14:25:30 +0200

    Changing compiler pluging to Java source 1.8

--- src/main/java/ici/netcompany/training/Application.java
+++ src/main/java/ici/netcompany/training/Application.java
@@ -1,14 +1,28 @@
 package ici.netcompany.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    
+
     public Application() {
         System.out.println ("Inside Application...");
     }
 
-    // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application...");
 		Application app = new Application();
+		app.greet();
+    }
+
+    public void greet(){
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+        greetings.add("Hola");
+        greetings.add("Alo");
+
+        for (String greeting: greetings){
+            System.out.println(greeting);
+        }
     }
 }
