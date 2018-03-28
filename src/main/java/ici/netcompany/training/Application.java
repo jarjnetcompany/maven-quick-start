577811b0d07d2d37cb130bfde47332309317e78e
Jose Alberto Romero Jimenez <jarjnetcompany@gmail.com>
2018-03-28 13:08:05 +0200

    Modified Application.java source file

--- src/main/java/ici/netcompany/training/Application.java
+++ src/main/java/ici/netcompany/training/Application.java
@@ -3,12 +3,12 @@ package ici.netcompany.training;
 public class Application {
     
     public Application() {
-        System.out.println ("Inside Application");
+        System.out.println ("Inside Application...");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
+    	System.out.println ("Starting Application...");
+		Application app = new Application();
     }
 }
